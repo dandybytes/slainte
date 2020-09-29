668971d4fc66bb5ea0b147a09832fd0c051d6dce
dandybytes <dandybytes@gmx.com>
2020-09-29 22:09:32 +0200

    Wrap beer glass emoticon in span tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,13 @@ const Header = ({handleButtonClick}) => {
     return (
         <header className='header'>
             <h1 className='title'>Slainte</h1>
-            <h2 className='subtitle'>the random 🍺 app</h2>
+            <h2 className='subtitle'>
+                the random{' '}
+                <span role='img' aria-label='foaming beer glass emoticon'>
+                    🍺
+                </span>{' '}
+                app
+            </h2>
             <button className='button' onClick={handleButtonClick}>
                 Pour me another one!
             </button>
